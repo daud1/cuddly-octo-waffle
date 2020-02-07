d662b8535a1b756c2576aaa012ca70bd33310738
David Mwebaza <david.mwebaza@andela.com>
2020-02-07 15:43:44 +0300

    Fix prop destructuring in Review component

--- src/components/Review.js
+++ src/components/Review.js
@@ -3,13 +3,13 @@ import PropTypes from "prop-types";
 import athenaLogo from "../images/sample_profile_pic.jpg";
 
 const Review = props => {
-  const { title, author, rating, description, date_posted } = props;
+  const { title, author, rating, description, date_posted } = props.review;
   return (
     <div className="review">
       <img src={athenaLogo} alt="profile" />
       <span>{title}</span>
       <span>{rating}</span>
-      {/** The rating is supposed to be a series of star icons */}
+      {/* The rating is supposed to be a series of star icons */}
       <span>{description}</span>
       <span>By: {author.name}</span>
       <span>{date_posted}</span>
