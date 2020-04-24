f98db176f720160e64e9760926c9ca53a7cff848
David Mwebaza <david.mwebaza@andela.com>
2020-04-24 12:34:39 +0300

    - Add reducers for Applications concern

--- src/employee/reducers.js
+++ src/employee/reducers.js
@@ -32,7 +32,7 @@ export const {
   editReviewSuccess,
 } = reviewsSlice.actions;
 
-export function fetchReviews(profileId, key) {
+export function getReviews(profileId, key) {
   return dispatch => {
     dispatch(setLoading({ isLoading: true }));
 
@@ -52,12 +52,12 @@ export function fetchReviews(profileId, key) {
   };
 }
 
-export function editReview(key, reviewEdits) {
+export function editReview(key, changeset) {
   return dispatch => {
     const headers = { "content-type": "application/json", Authorization: `Token ${key}` };
 
     return axios
-      .patch(`${API_URL}/employee/reviews/${reviewEdits.id}/`, reviewEdits, { headers })
+      .patch(`${API_URL}/employee/reviews/${changeset.id}/`, changeset, { headers })
       .then(response => {
         dispatch(editReviewSuccess({ review: response.data }));
         dispatch(setLoading({ isLoading: false }));
@@ -184,16 +184,14 @@ export function fetchQualifications(profileId, key) {
       });
   };
 }
-export function editQualification(key, qualificationEdits) {
+export function editQualification(key, changeset) {
   return dispatch => {
     const headers = { "content-type": "application/json", Authorization: `Token ${key}` };
 
     return axios
-      .patch(
-        `${API_URL}/employee/qualifications/${qualificationEdits.id}/`,
-        qualificationEdits,
-        { headers }
-      )
+      .patch(`${API_URL}/employee/qualifications/${changeset.id}/`, changeset, {
+        headers,
+      })
       .then(response => {
         dispatch(editQualificationSuccess({ qualification: response.data }));
         dispatch(setLoading({ isLoading: false }));
@@ -227,7 +225,103 @@ export function addQualification(profileId, key, qualification) {
   };
 }
 
+const applicationsSlice = createSlice({
+  name: "applications",
+  initialState: { applications: [], error: null },
+  reducers: {
+    fetchApplicationsSuccess(state, action) {
+      state.applications = action.payload.applications;
+    },
+    fetchApplicationsError(state, action) {
+      state.error = action.payload.error;
+    },
+    editApplicationSuccess(state, action) {
+      const { application } = action.payload;
+      let index = state.applications.findIndex(element => element.id === application.id);
+      state.applications[index] = application;
+    },
+    editApplicationError(state, action) {
+      state.error = action.payload.error;
+    },
+    addApplicationSuccess(state, action) {
+      state.applications.push(action.payload.application);
+    },
+    addApplicationError(state, action) {
+      state.error = action.payload.error;
+    },
+  },
+});
+
+export function fetchApplications(profileId, key) {
+  return dispatch => {
+    const headers = { "content-type": "application/json", Authorization: `Token ${key}` };
+
+    dispatch(setLoading({ isLoading: true, loadingText: "Fetching Your Data..." }));
+    return axios
+      .get(`${API_URL}/employee/applications/?employee=${profileId}`, { headers })
+      .then(response => {
+        dispatch(fetchApplicationsSuccess({ applications: response.data }));
+        dispatch(setLoading({ isLoading: false }));
+      })
+      .catch(error => {
+        dispatch(setLoading({ isLoading: false }));
+        dispatch(fetchApplicationsError({ error: error.data }));
+        showAPIErrors(error, setNotification);
+      });
+  };
+}
+
+export function editApplication(key, changeset) {
+  return dispatch => {
+    const headers = { "content-type": "application/json", Authorization: `Token ${key}` };
+
+    return axios
+      .patch(`${API_URL}/employee/applications/${changeset.id}/`, changeset, { headers })
+      .then(response => {
+        dispatch(editApplicationSuccess({ application: response.data }));
+        dispatch(setLoading({ isLoading: false }));
+        dispatch(setNotification({ message: "Changes Saved!" }));
+      })
+      .catch(error => {
+        dispatch(setLoading({ isLoading: false }));
+        dispatch(editApplicationError({ error: error.data }));
+        showAPIErrors(error, setNotification);
+      });
+  };
+}
+
+export function applyForJob(profileId, key, application) {
+  return dispatch => {
+    const headers = { "content-type": "application/json", Authorization: `Token ${key}` };
+    application.employee = profileId;
+
+    dispatch(setLoading({ isLoading: true, loadingText: "Working..." }));
+    return axios
+      .post(`${API_URL}/employee/applications/`, application, { headers })
+      .then(response => {
+        dispatch(addApplicationSuccess({ application: response.data }));
+        dispatch(setLoading({ isLoading: false }));
+        dispatch(setNotification({ message: "Application added!" }));
+      })
+      .catch(error => {
+        dispatch(setLoading({ isLoading: false }));
+        dispatch(addApplicationError({ error: error.data }));
+        showAPIErrors(error, setNotification);
+      });
+  };
+}
+
+export const {
+  fetchApplicationsSuccess,
+  fetchApplicationsError,
+  editApplicationError,
+  editApplicationSuccess,
+  addApplicationError,
+  addApplicationSuccess,
+} = applicationsSlice.actions;
+
 export default combineReducers({
+  applicatons: applicationsSlice.reducer,
   reviews: reviewsSlice.reducer,
   qualifications: qualificationsSlice.reducer,
 });
